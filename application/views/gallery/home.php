6042a7dbb9c06e3ca0bff5aaa0a4678329de2c85
Shoichiinaba <inabashoichi01@gmail.com>
2022-02-14 13:22:31 +0700

    update final V.1.0

--- application/views/gallery/home.php
+++ application/views/gallery/home.php
@@ -1,34 +1,46 @@
-<section class="content">
-    <div class="row clearfix">
-        <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
-            <div class="panel-group" id="accordion_17" role="tablist" aria-multiselectable="true">
-                <div class="panel panel-col-purple">
-                    <div class="panel-heading" role="tab" id="headingOne_17">
-                        <h4 class="panel-title">
-                            <a role="button" data-toggle="collapse" data-parent="#accordion_17" href="#collapseOne_17" aria-expanded="true" aria-controls="collapseOne_17">
-                                <center><img src="<?php echo base_url('assets/'); ?>images/lg3.ico" width="48" height="48" alt="User"> Pilih Toko Untuk Melihat Gallery</center>
-                            </a>
-                        </h4>
-                    </div>
-                    <div id="collapseOne_17" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne_17">
-                        <div class="panel-body">
-                            <div class="row clearfix">
-                                <?php $no = 0;
-                                foreach ($toko as $g) : $no++; ?>
-                                    <a href="<?php echo site_url('Manage_gallery/katalog/' . $g->id); ?>">
-                                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 p-b-10">
-                                            <button class="btn bg-purple btn-lg btn-block waves-effect" type="button">
-                                                <i class=" material-icons">store</i>
-                                                <span><?php echo $g->nama_toko; ?></span>
-                                            </button>
-                                        </div>
-                                    </a>
-                                <?php endforeach; ?>
+<?php
+echo "<style>
+    body{
+        background-image:url(" . base_url() . "assets/images/home_bg/bg2.jpg);
+        background-attachment:fixed;
+        background-size:100% 100%;
+    }
+ </style>";
+?>
+
+<body>
+    <section class="content">
+        <div class="row clearfix">
+            <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+                <div class="panel-group" id="accordion_17" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-col-purple">
+                        <div class="panel-heading" role="tab" id="headingOne_17">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion_17" href="#collapseOne_17" aria-expanded="true" aria-controls="collapseOne_17">
+                                    <center><img src="<?php echo base_url('assets/'); ?>images/lg3.ico" width="48" height="48" alt="User"> Pilih Toko Untuk Melihat Gallery</center>
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne_17" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne_17">
+                            <div class="panel-body">
+                                <div class="row clearfix">
+                                    <?php $no = 0;
+                                    foreach ($toko as $g) : $no++; ?>
+                                        <a href="<?php echo site_url('Manage_gallery/katalog/' . $g->id); ?>">
+                                            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 p-b-10">
+                                                <button class="btn bg-purple btn-lg btn-block waves-effect" type="button">
+                                                    <i class=" material-icons">store</i>
+                                                    <span><?php echo $g->nama_toko; ?></span>
+                                                </button>
+                                            </div>
+                                        </a>
+                                    <?php endforeach; ?>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+</body>
